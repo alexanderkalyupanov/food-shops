54960a00178b389e875390958f67eb90c080387c
alexanderkalyupanov <138789735+alexanderkalyupanov@users.noreply.github.com>
2024-06-13 18:44:38 +0300

    add lite slider

--- js/script.js
+++ js/script.js
@@ -376,4 +376,52 @@ window.addEventListener("DOMContentLoaded", () => {
         .then(data => data.json())
         .then(res => console.log(res))
 
+
+    const slides = document.querySelectorAll(".offer__slide"),
+        slidePrev = document.querySelector(".offer__slider-prev"),
+        slideNext = document.querySelector(".offer__slider-next"),
+        current = document.querySelector("#current"),
+        total = document.querySelector("#total");
+
+
+    let slideIndex = 1;
+    
+    showSlides(slideIndex)
+
+    if (slides.length < 10) {
+        total.textContent = `0${slides.length}`
+    } else {
+        total.textContent = slides.length
+    }
+
+    function showSlides(n) {
+        if (n > slides.length) {
+            slideIndex = 1;
+        } 
+        if (n < 1) {
+            slideIndex = slides.length
+        }
+
+        slides.forEach(item => item.style.display = "none")
+
+        slides[slideIndex - 1].style.display = "block";
+
+        
+    if (slides.length < 10) {
+        current.textContent = `0${slideIndex}`
+    } else  {
+        current.textContent = slideIndex
+    }
+    }
+
+    function plusSlides(n) {
+        showSlides(slideIndex += n)
+    }
+
+    slidePrev.addEventListener("click", () => {
+        plusSlides(-1)
+    })
+    slideNext.addEventListener("click", () => {
+        plusSlides(1)
+    })
 });
